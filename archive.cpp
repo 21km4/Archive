d22cbf0ffc8067c8efbd4f2dc88517c3128a926c
水本幸希 <48169975+21KM4@users.noreply.github.com>
2021-12-25 15:15:32 +0900

    Add files via upload

--- archive.cpp
+++ archive.cpp
@@ -216,7 +216,7 @@ size_t GetDataFromArchive(std::string path, void* dest, std::string archive_path
 	std::vector<std::string> paths;
 	ARCHIVE_HEADER header;
 	size_t head_size = ReadHeader(ifs, header, head, paths);
-	if (head_size == 0) return 0;
+	if (head_size == 0) return false;
 
 	std::string first_dir;
 	if (header.is_directory) first_dir = path.substr(0, pos + 1);
@@ -238,9 +238,9 @@ size_t GetDataFromArchive(std::string path, void* dest, std::string archive_path
 			SHA3_384((uint8_t*)paths[i].c_str(), paths[i].size(), hash);
 			SHA3_384((uint8_t*)password.c_str(), password.size(), pass);
 			for (int i = 0; i < 48; i++) hash[i] ^= pass[i];
+
 			AesCtx ctx;
 			AesInitKey(&ctx, hash, 32);
-
 			if (header.is_encrypted) head[i].pressed_size = AesDecryptCbc(&ctx, hash + 32, pressed, head[i].pressed_size);
 			uncompress(original, (uLongf*)&head[i].original_size, pressed, (uLongf)head[i].pressed_size);
 
@@ -284,9 +284,8 @@ bool DecodeArchive(std::string path)
 	for (size_t i = 0; i < head.size(); i++)
 	{
 		uint8_t* original = new uint8_t[head[i].original_size];
-		size_t s;
-		if (header.is_directory) s = GetDataFromArchive(paths[i], original);
-		else s = GetDataFromArchive(paths[i], original, path);
+		if (header.is_directory) GetDataFromArchive(paths[i], original);
+		else GetDataFromArchive(paths[i], original, path);
 
 		std::cout << paths[i] << std::endl;
 		std::cout << "size: " << head[i].original_size << " Byte" << std::endl;
--- archive.h
+++ archive.h
@@ -17,4 +17,9 @@ void SetArchiveExtension(const std::string& _extension);
 bool EncodeArchive(std::string path, int _compress_level = Z_DEFAULT_COMPRESSION, bool _encrypt = true);
 bool DecodeArchive(std::string path);
 bool CheckArchive(std::string path);
-size_t GetDataFromArchive(std::string path, void* dest, std::string archive = "");
+
+// 第一引数　ファイルのパス（ディレクトリを圧縮した場合はアーカイブファイルの拡張子を除いた部分が最初のディレクトリとなる）
+// 第二引数　ファイルデータを受け取るバッファ（あらかじめ確保すること）。NULLやnullptrを指定すればデータサイズのみが返される。
+// 第三引数　アーカイブファイルのパス（ディレクトリを圧縮した場合は無視される）アーカイブファイル名の拡張子を除いた部分が圧縮したファイル名と同じなら省略可。
+// 戻り値　　データサイズ。パスワードが間違ったりファイルが存在しないときは０を返す。
+size_t GetDataFromArchive(std::string path, void* dest, std::string archive_path = "");
